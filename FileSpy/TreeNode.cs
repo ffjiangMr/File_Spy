6f7488d200db1dfd3665e49b5f72177f9b16eb2d
Feifei Jiang <Feifei.Jiang@outlook.com>
2019-11-26 15:26:02 +0800

    updata

---
+++ FileSpy/TreeNode.cs
@@ -0,0 +1,138 @@
+﻿namespace FileSpy
+{
+    #region using directive
+
+    using System;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+
+    #endregion
+
+    public sealed class TreeNode : INotifyPropertyChanged
+    {
+        public TreeNode(TreeNode parent)
+        {
+            this.parentNode = parent;
+        }
+
+        public String Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+                this.OnProperty("Name");
+            }
+        }
+
+        public String Size
+        {
+            get
+            {
+                String unit = "B";
+                Double size = this.Length;
+                while (size > 1024.0)
+                {
+                    size = size / 1024.0;
+                    switch (unit)
+                    {
+                        case "B":
+                            unit = "KB";
+                            break;
+                        case "KB":
+                            unit = "MB";
+                            break;
+                        case "MB":
+                            unit = "GB";
+                            break;
+                    }
+                }
+                var result = size.ToString("F4") + " " + unit;
+                if (size == 0)
+                {
+                    result = "****";
+                }
+                return result;
+            }
+            set
+            {
+                this.OnProperty("Size");
+            }
+        }
+
+        public Int64 Length
+        {
+            get
+            {
+                return this.length;
+            }
+            set
+            {
+                this.length = value;
+            }
+        }
+
+        public ObservableCollection<TreeNode> ChildNodes
+        {
+            get
+            {
+                return this.childNodes;
+            }
+            set
+            {
+                this.childNodes = value;
+                this.OnProperty("ChildNode");
+            }
+        }
+
+        public String Image
+        {
+            get
+            {
+                var result = String.Empty;
+                if (this.IsDevice)
+                {
+                    result = @"images\device.ico";
+                }
+                else if (this.IsFolder)
+                {
+                    result = @"images\folder.ico";
+                }
+                else if (this.IsFile)
+                {
+                    result = @"images\file.ico";
+                }
+                return result;
+            }
+        }
+
+        public String Path { get; set; }
+
+        public TreeNode ParentNode { get { return this.parentNode; } }
+
+        public bool IsFile { get; set; }
+        public bool IsFolder { get; set; }
+        public bool IsDevice { get; set; }
+
+        private void OnProperty(in String propertyName)
+        {
+            var temp = this.PropertyChanged;
+            if (temp != null)
+            {
+                temp(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private String name = String.Empty;
+
+        private Int64 length = 0;
+
+        private ObservableCollection<TreeNode> childNodes = new ObservableCollection<TreeNode>();
+        private readonly TreeNode parentNode;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ FileSpy/WindowDataContext.cs
@@ -0,0 +1,111 @@
+﻿
+namespace FileSpy
+{
+    #region  using directive
+
+    using System;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    #endregion
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+
+        public ObservableCollection<TreeNode> Source
+        {
+            get
+            {
+                return this.source;
+            }
+            set
+            {
+                this.source = value;
+                this.OnPropertyChanged("Source");
+            }
+        }
+
+        private void TraversingDevice()
+        {
+            var devices = Environment.GetLogicalDrives();
+            foreach (var device in devices)
+            {
+                var temp = new TreeNode(null)
+                {
+                    Name = device.Substring(0, device.IndexOf(':')),
+                    IsDevice = true,
+                    Path = device,
+                };
+                this.Dispatcher.Invoke(new Action(() =>
+                {
+                    this.source.Add(temp);
+                }));
+                Task task = new Task(() =>
+                {
+                    this.TraversingFolder(temp);
+                });
+                task.Start();
+            }
+        }
+
+        private Int64 TraversingFolder(TreeNode path)
+        {
+            Int64 result = 0;
+            try
+            {
+                foreach (var folder in Directory.GetDirectories(path.Path))
+                {
+                    var temp = new TreeNode(path)
+                    {
+                        Name = folder.Substring(folder.Replace('\\', '/').LastIndexOf('/') + 1),
+                        IsFolder = true,
+                        Path = folder,
+                    };
+                    this.Dispatcher.Invoke(new Action(() =>
+                    {
+                        path.ChildNodes.Add(temp);
+                    }));
+                    result += this.TraversingFolder(temp);
+                }
+                foreach (var file in Directory.GetFiles(path.Path))
+                {
+                    var temp = new TreeNode(path)
+                    {
+                        Name = file.Substring(file.Replace('\\', '/').LastIndexOf('/') + 1),
+                        IsFile = true,
+                        Path = file,
+                    };
+                    this.Dispatcher.Invoke(new Action(() =>
+                    {
+                        path.ChildNodes.Add(temp);
+                    }));
+                    FileInfo info = new FileInfo(file);
+                    temp.Length = info.Length;
+                    result += info.Length;
+                }
+                path.Length = result;
+                path.Size = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<TreeNode> source = new ObservableCollection<TreeNode>();
+        private void OnPropertyChanged(in String propertyName)
+        {
+            var temp = this.PropertyChanged;
+            if (temp != null)
+            {
+                temp(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
